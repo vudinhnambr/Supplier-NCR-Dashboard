e808f60228e927ce8178a7dc5b0fede1da94bafc
vudinhnambr <153347354+vudinhnambr@users.noreply.github.com>
2026-06-05 13:04:48 +0700

    Update ncr.js

--- api/ncr.js
+++ api/ncr.js
@@ -1,40 +1,23 @@
 import axios from "axios";
-import * as XLSX from "xlsx";
 
 export default async function handler(req, res) {
 
   try {
 
-    const fileUrl =
-      process.env.NCR_XLSX_URL;
-
     const response =
-      await axios.get(fileUrl, {
-        responseType: "arraybuffer"
-      });
-
-    const workbook =
-      XLSX.read(response.data, {
-        type: "buffer"
-      });
-
-    const worksheet =
-      workbook.Sheets["Sheet1"];
-
-    const rows =
-      XLSX.utils.sheet_to_json(
-        worksheet,
-        {
-          defval: ""
-        }
+      await axios.get(
+        process.env.NCR_XLSX_URL
       );
 
-    return res.status(200).json(rows);
+    return res.status(200).json({
+      first100:
+        response.data.substring(0,100)
+    });
 
-  } catch (error) {
+  } catch(error) {
 
     return res.status(500).json({
-      error: error.message
+      error:error.message
     });
 
   }
