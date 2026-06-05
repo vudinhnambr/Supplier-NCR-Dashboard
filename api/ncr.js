634c144a87de9c25564e397b75f54cd2945cecd6
vudinhnambr <153347354+vudinhnambr@users.noreply.github.com>
2026-06-05 12:41:29 +0700

    Update ncr.js

--- api/ncr.js
+++ api/ncr.js
@@ -1,6 +1,23 @@
-export default function handler(req, res) {
-  return res.status(200).json({
-    status: "OK",
-    message: "API is working"
-  });
+import axios from "axios";
+
+export default async function handler(req, res) {
+
+  try {
+
+    const fileUrl =
+      process.env.NCR_XLSX_URL;
+
+    return res.status(200).json({
+      status: "STEP1_OK",
+      fileUrl
+    });
+
+  } catch (error) {
+
+    return res.status(500).json({
+      error: error.message
+    });
+
+  }
+
 }
